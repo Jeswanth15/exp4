a903a5e53b4961961c1914ccfbdd52c3ecc86e70
neojarvis <techadmin@iamneo.ai>
2025-03-14 07:17:30 +0000

    Submit-project-save-commit-3/14/2025--12:47:30-PM

--- springapp/src/main/java/com/examly/springapp/repository/EmployeeRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/EmployeeRepo.java
@@ -1,20 +1,20 @@
-package com.examly.springapp.repository;
+    package com.examly.springapp.repository;
 
-import java.util.List;
+    import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.stereotype.Repository;
 
-import com.examly.springapp.model.Employee;
+    import com.examly.springapp.model.Employee;
 
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-        
-      
-        List<Employee> findByNameContainingIgnoreCaseOrDesignationContainingIgnoreCase(String name, String designation);
-            
-            List<Employee> findByNameIsContainingIgnoreCase(String searchTerm);
-                List<Employee> findByNameStartsWithIgnoreCase(String prefix);
-                    List<Employee> findByNameEndsWithIgnoreCase(String suffix);
-                        List<Employee> findByDesignationContainingIgnoreCase(String designation);
-                        }
+    @Repository
+    public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+
+
+    List<Employee> findByNameContainingIgnoreCaseOrDesignationContainingIgnoreCase(String name, String designation);
+
+    List<Employee> findByNameIsContainingIgnoreCase(String searchTerm);
+    List<Employee> findByNameStartsWithIgnoreCase(String prefix);
+    List<Employee> findByNameEndsWithIgnoreCase(String suffix);
+    List<Employee> findByDesignationContainingIgnoreCase(String designation);
+    }
